ff0306aaa074a1a03a3eb2f0e62d3d0780d32ede
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-11-10 14:39:30 +0630

    update

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+	.vam {
+		display: flex;
+	}
+
+	.d1 {
+		width: 200px;
+		height: 200px;
+		/* background-color: red; */
+	}
+
+	.d2 {
+		width: 200px;
+		height: 200px;
+		/* background-color: blue; */
+	}
+</style>
+
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 	<title>Document</title>
 </head>
 
@@ -16,10 +36,27 @@
 
 	<h5><?php echo $age ?></h5>
 
+	<div class="vam">
+		<div class="d1">
+			wefwef
+		</div>
+		<div class="d2">
+			ffffffffff
+		</div>
+	</div>
+	<button onclick="fun()">CLICK</button>
+
 </body>
 
 </html>
 
+
+<script>
+	function fun() {
+		$(".d1").css("background", "purple");
+		$(".d2").css("background", "purple");
+	}
+</script>
 
 
 <?php
